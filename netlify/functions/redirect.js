d299291f5055455b031a59cb2eae7ef9e23e5b1d
Vladimir V.M <vvmyagdeev@gmail.com>
2024-06-08 03:06:47 +0400

    fix<js>: path

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -1,18 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
 exports.handler = async function(event, context) {
-    const redirectTo = process.env.REDIRECT_TO || "https://hamster2.top/";
-    
-    if (path === "/" || path === "/index.html") {
-        return {
-          statusCode: 200,
-          body: "This is the root page, no redirection."
-        };
-    }
-    
+  const redirectTo = process.env.REDIRECT_TO || "https://example.com";
+  const requestPath = event.path;
+
+  // Do not redirect the root path
+  if (requestPath === "/" || requestPath === "/index.html") {
+    const indexPath = path.resolve(__dirname, '../../index.html');
+    const indexContent = fs.readFileSync(indexPath, 'utf8');
+
     return {
-      statusCode: 301,
-      headers: {
-        Location: redirectTo,
-      },
+      statusCode: 200,
+      headers: { 'Content-Type': 'text/html' },
+      body: indexContent
     };
+  }
+
+  return {
+    statusCode: 301,
+    headers: {
+      Location: redirectTo,
+    },
   };
-  
+};
