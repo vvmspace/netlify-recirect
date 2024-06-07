3218b94715a656421ca4806eadb8aceaea03a0f0
Vladimir V.M <vvmyagdeev@gmail.com>
2024-06-08 03:18:09 +0400

    fix<debug>: getting netllify function data

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -5,16 +5,12 @@ exports.handler = async function(event, context) {
   const redirectTo = process.env.REDIRECT_TO || "https://example.com";
   const requestPath = event.path;
 
-  // Do not redirect the root path
-  if (requestPath === "/" || requestPath === "/index.html") {
-    const indexPath = path.resolve(__dirname, '../../index.html');
-    const indexContent = fs.readFileSync(indexPath, 'utf8');
-
-    return {
-      statusCode: 200,
-      headers: { 'Content-Type': 'text/html' },
-      body: indexContent
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+        event,
+        context,
+    })
   }
 
   return {
