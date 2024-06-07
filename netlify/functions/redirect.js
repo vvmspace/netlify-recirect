048067c86d50d1823205d7c9a9eeae009a3fdf54
Vladimir V.M <vvmyagdeev@gmail.com>
2024-06-08 03:32:53 +0400

    feat: noxyu

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -1,16 +1,4 @@
-const fs = require("fs");
-const { join } = require("path");
-
 exports.handler = async function (event, context) {
-  const path = event.path.split("/redirect")[1];
-
-  if (!path || path === "/" || path === "/index.html") {
-    return {
-      statusCode: 200,
-      body: fs.readFileSync(join(__dirname, "../..", "index.html")).toString(),
-    };
-  }
-
   return {
     statusCode: 301,
     headers: {
