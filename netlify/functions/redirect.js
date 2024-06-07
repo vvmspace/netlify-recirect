b3324a0119e51e4c7348a2a81919f1590376acc7
Vladimir V.M <vvmyagdeev@gmail.com>
2024-06-08 02:59:36 +0400

    feat</>: no redirect root

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -1,5 +1,13 @@
 exports.handler = async function(event, context) {
     const redirectTo = process.env.REDIRECT_TO || "https://hamster2.top/";
+    
+    if (path === "/" || path === "/index.html") {
+        return {
+          statusCode: 200,
+          body: "This is the root page, no redirection."
+        };
+    }
+    
     return {
       statusCode: 301,
       headers: {
